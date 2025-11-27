3131471fc9de0716e4bdcac2293957ee0e0cd167
Ink <natthakul.yis@student.mahidol.edu>
2025-11-28 01:28:46 +0700

    feat: create fn_customer_register

---
+++ fn_customer_register.sql
@@ -0,0 +1,26 @@
+create function fn_customer_register(p_shop_identifier text, p_email text, p_password text, p_first_name text, p_last_name text) returns integer
+    language plpgsql
+as
+$$
+DECLARE
+    v_shop_id INT;
+    v_new_customer_id INT;
+BEGIN
+    SELECT shop_id INTO v_shop_id
+    FROM client
+    WHERE shop_name = p_shop_identifier
+    LIMIT 1;
+
+    INSERT INTO shop_customer (shop_id, first_name, last_name, email, password_hash)
+    VALUES (v_shop_id, p_first_name, p_last_name, p_email, p_password)
+    RETURNING customer_id INTO v_new_customer_id;
+
+    RETURN v_new_customer_id;
+
+EXCEPTION
+    WHEN unique_violation THEN
+        RAISE EXCEPTION 'Registration failed: An account with this email already exists for shop %.', p_shop_identifier;
+END;
+$$;
+
+alter function fn_customer_register(text, text, text, text, text) owner to root;
