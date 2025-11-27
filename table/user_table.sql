ef0337e13c2dfa96bfd73abdba8f280ec5944ded
u6680972 <natthakul.yis@student.mahidol.edu>
2025-11-28 02:52:34 +0700

    fix: add user_type

--- table/user_table.sql
+++ table/user_table.sql
@@ -4,3 +4,12 @@ CREATE TABLE "user" (
     password_hash TEXT NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL
 );
+
+ALTER TABLE "user" ADD COLUMN first_name VARCHAR(100);
+ALTER TABLE "user" ADD COLUMN last_name VARCHAR(100);
+ALTER TABLE "user" ALTER COLUMN first_name SET NOT NULL;
+ALTER TABLE "user" ALTER COLUMN last_name SET NOT NULL;
+
+ALTER TABLE "user" ADD COLUMN user_type VARCHAR(20) DEFAULT 'manager';
+ALTER TABLE "user" ALTER COLUMN user_type SET NOT NULL;
+ALTER TABLE "user" ADD CONSTRAINT check_valid_user_type CHECK (user_type IN ('manager', 'customer'));
