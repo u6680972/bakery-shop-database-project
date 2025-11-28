716ea8aac31c2137c723b4cc9de7736698fdc98f
u6680972 <natthakul.yis@student.mahidol.edu>
2025-11-29 01:35:17 +0700

    fix: add columns

--- table/order_item_table.sql
+++ table/order_item_table.sql
@@ -1,20 +1,20 @@
 create table order_item
 (
-    order_item_id serial
+    order_item_id       serial
         primary key,
-    order_id      integer not null
+    order_id            integer not null
         constraint fk_order_item_order
             references "order"
             on delete cascade,
-    product_name  text    not null,
-    quantity      integer                  default 1,
-    price         numeric(10, 2),
-    notes         text,
-    created_at    timestamp with time zone default CURRENT_TIMESTAMP
+    product_name        text    not null,
+    quantity            integer                  default 1,
+    price               numeric(10, 2),
+    notes               text,
+    created_at          timestamp with time zone default CURRENT_TIMESTAMP,
+    reference_image_url text
 )
     using ???;
 
 alter table order_item
     owner to root;
-
 
