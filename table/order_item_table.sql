f58d121fd056f8d6b817dad166afe8becaf3fec6
u6680972 <natthakul.yis@student.mahidol.edu>
2025-11-28 23:17:14 +0700

    feat: create order_item_table

---
+++ table/order_item_table.sql
@@ -0,0 +1,20 @@
+create table order_item
+(
+    order_item_id serial
+        primary key,
+    order_id      integer not null
+        constraint fk_order_item_order
+            references "order"
+            on delete cascade,
+    product_name  text    not null,
+    quantity      integer                  default 1,
+    price         numeric(10, 2),
+    notes         text,
+    created_at    timestamp with time zone default CURRENT_TIMESTAMP
+)
+    using ???;
+
+alter table order_item
+    owner to root;
+
+
