287e4cb527105283208cba77bacb74f27d823c6c
u6680972 <natthakul.yis@student.mahidol.edu>
2025-11-28 23:31:28 +0700

    fix: add columns

--- table/product_table.sql
+++ table/product_table.sql
@@ -1,15 +1,19 @@
 create table product
 (
-    product_id  serial
+    product_id     serial
         primary key,
-    shop_id     integer not null
+    shop_id        integer not null
         constraint fk_product_shop
             references client,
-    name        text    not null,
-    description text,
-    price       numeric(10, 2),
-    is_active   boolean                  default true,
-    created_at  timestamp with time zone default CURRENT_TIMESTAMP
+    name           text    not null,
+    description    text,
+    price          numeric(10, 2),
+    is_active      boolean                  default true,
+    created_at     timestamp with time zone default CURRENT_TIMESTAMP,
+    category       text,
+    stock_quantity integer                  default 0,
+    image_url      text,
+    updated_at     timestamp with time zone default CURRENT_TIMESTAMP
 )
     using ???;
 
