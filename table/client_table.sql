bb4a56670316cb484bc59ee7551c813828bee96c
u6680972 <natthakul.yis@student.mahidol.edu>
2025-11-30 01:46:10 +0700

    fix: add column for subscription

--- table/client_table.sql
+++ table/client_table.sql
@@ -1,18 +1,25 @@
 create table client
 (
-    shop_id      serial
+    shop_id                 serial
         primary key,
-    owner_id     integer      not null
+    owner_id                integer      not null
         unique
         references manager_user
             on delete restrict,
-    shop_name    varchar(100) not null
+    shop_name               varchar(100) not null
         unique,
-    phone_number text,
-    email        text,
-    address      text
+    phone_number            text,
+    email                   text,
+    address                 text,
+    plan_name               text           default 'Starter'::text,
+    plan_price              numeric(10, 2) default 29.99,
+    billing_cycle           text           default 'Monthly'::text,
+    next_billing_date       date,
+    is_auto_renewal_enabled boolean        default true,
+    subscription_status     text           default 'Active'::text
 )
     using ???;
 
 alter table client
     owner to root;
+
